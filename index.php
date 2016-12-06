4f2739e0255ebe997d6fa83ec0fd14a50c5bc0bc
abby <abbigail.abas@gmail.com>
2016-12-06 17:34:24 +0000

    made changes lol

--- index.php
+++ index.php
@@ -2,17 +2,26 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Document</title>
+  <title>WebD Squad</title>
   <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-  <h1 class="h1-header"> I KNOW WHEN THAT HOTLINE BLING </h1>  
   
+<!--   <h1 class="h1-header"> I KNOW WHEN THAT HOTLINE BLING </h1>   -->
+  <h1 class="h1-header">i know when that hotline bling</h1>
+  
+  <!--  how to include jquery  -->
+  <script src="/vendor/components/jquery/jquery.min.js"></script>
+
+  <!--  to test jquery working on the site  -->
   <script>
-  $(document)ready(function(){
+  $(document).ready(function(){
     console.log( 'jquery is working!' );
   })
   </script>
-</body>
+
+  
+<!--   list of all the packages available for json https://packagist.org/ -->
+  </body>
 </html>
 
